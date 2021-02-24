74edc877a315e12e454e64508e978603ac173ed9
= <=>
2021-02-24 02:52:22 -0600

    Made a Mortgage Calculator

--- src/furqan/furqanClass.java
+++ src/furqan/furqanClass.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class furqanClass {
 
-// Declaring States .
+    // Declaring States .
     public int currentYear = 2021;
     public String name;
     public int birthYear = 1983;
@@ -16,20 +16,20 @@ public class furqanClass {
     public String country;
     public String currentCity;
 
-// Declaring Behaviours:
-    public void displayName(){
+    // Declaring Behaviours:
+    public void displayName() {
         System.out.println("Hi. My name is " + name);
     }
 
-    public void displayAge(){
+    public void displayAge() {
         System.out.println("I am " + age + " years old.");
     }
 
-    public void displayRest(){
+    public void displayRest() {
         System.out.println("I am " + height + "ft tall and my star sign is " + birthSign);
     }
 
-    public void displayLocation(){
+    public void displayLocation() {
         System.out.println("I am originally from " + country + ". Right now I am based in " + currentCity);
     }
 }
